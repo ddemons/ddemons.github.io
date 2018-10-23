af97c531e5be2e9666e93450f99d4281455507d5
Matt Edmonds <edmondsmj@gmail.com>
2018-10-22 23:53:28 -0400

    api updates

--- Services/StarWarsApiService.js
+++ Services/StarWarsApiService.js
@@ -1,21 +1,61 @@
-app.factory('starWarsApiService', function($http){
+app.factory('starWarsApiService', function ($http, toastService) {
 
 
-    var getStarWarsPeople = function(pageNum) {
-        return $http.get('https://swapi.co/api/people/?page=' + pageNum ,{cache: true}).then(function(response){
+    var getStarWarsPeople = function (pageNum) {
+        return $http.get('https://swapi.co/api/people/?page=' + pageNum, {cache: true}).then(function (response) {
             return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
         });
     };
 
-    // var get = function(pageNum) {
-    //     return $http.get('https://swapi.co/api/people/?page=' + pageNum ,{cache: true}).then(function(response){
-    //         return response.data;
-    //     });
-    // };
+    var getStarWarsShips = function (pageNum) {
+        return $http.get('https://swapi.co/api/starships/?page=' + pageNum, {cache: true}).then(function (response) {
+            return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
+        });
+    };
+
+    var getStarWarsIndividualPerson = function (id) {
+        return $http.get('https://swapi.co/api/people/' + id + '/', {cache: true}).then(function (response) {
+            return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
+        });
+    };
+
+    var getStarWarsIndividualPlanet = function (id) {
+        return $http.get('https://swapi.co/api/planets/' + id + '/', {cache: true}).then(function (response) {
+            return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
+        });
+    };
+
+    var getStarWarsIndividualStarShip = function (id) {
+        return $http.get('https://swapi.co/api/starships/' + id + '/', {cache: true}).then(function (response) {
+            return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
+        });
+    };
+
+    var getStarWarsIndividualFilm = function (id) {
+        return $http.get('https://swapi.co/api/films/' + id + '/', {cache: true}).then(function (response) {
+            return response.data;
+        }).catch(function (response){
+            toastService.errorToast("Something went wrong. Please refresh or try again later.");
+        });
+    };
 
 
     return {
-        getStarWarsPeople: getStarWarsPeople
-
+        getStarWarsPeople: getStarWarsPeople,
+        getStarWarsShips: getStarWarsShips,
+        getStarWarsIndividualPerson: getStarWarsIndividualPerson,
+        getStarWarsIndividualPlanet: getStarWarsIndividualPlanet,
+        getStarWarsIndividualStarShip: getStarWarsIndividualStarShip,
+        getStarWarsIndividualFilm: getStarWarsIndividualFilm
     };
 });
